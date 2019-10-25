c9bc7451229e33fb21b25e7d07fff5718c8f259d
Seb Dalgarno <seb@poissonconsulting.ca>
2019-10-25 15:53:50 -0700

    config file, warn user how many files will be modified

--- R/app_server.R
+++ R/app_server.R
@@ -1,10 +1,6 @@
 app_server <- function(input, output, session) {
   
-  shinyFiles::shinyDirChoose(
-    input,
-    'dir',
-    roots = c(home = '~')
-  )
+  shinyFiles::shinyDirChoose(input, 'dir', roots = c(home = '~'))
   
   global <- reactiveValues(datapath = getwd())
   
@@ -20,17 +16,39 @@ app_server <- function(input, output, session) {
                             collapse = .Platform$file.sep))
   })
   
-  observeEvent(input$go, {
-    withProgress(batchr::batch_gsub(pattern = input$pattern, replacement = input$replacement,
-                                    path = global$datapath, regexp = input$regexp,
-                                    recurse = input$recurse, progress = FALSE, report = FALSE, 
-                                    ask = FALSE), value = 0.5, 
-                 message = "completing task...")
-    updateTextInput(session, "pattern", value = "")
-    updateTextInput(session, "replacement", value = "")
+  observeEvent(input$done, {
+    path <- global$datapath
+    if (length(batchr:::config_files(path, recursive = FALSE))) {
+      batchr::batch_reconfig_fun(batchr::gsub_file, pattern = input$pattern,
+                                 replacement = input$replacement, path = path)
+    } else {
+      batchr::batch_config(batchr::gsub_file, pattern = input$pattern,
+                           replacement = input$replacement, path = path,
+                           regexp = input$regexp, recurse = input$recurse)
+    }
+
+    remaining <- batchr::batch_files_remaining(path)
     
-    showModal(modalDialog(h5("Task complete"), footer = modalButton("Got it")))
+    modal <- modalDialog(h5(paste(length(remaining), "files will be modified")),
+                p(paste(remaining, collapse = ", ")),
+                footer = modalButton("Go for it"))
     
+    showModal(modal)
+    
+    
+    
+    # withProgress(batchr::batch_gsub(pattern = input$pattern, replacement = input$replacement,
+    #                                 path = global$datapath, regexp = input$regexp,
+    #                                 recurse = input$recurse, progress = FALSE, report = FALSE, 
+    #                                 ask = FALSE), value = 0.5, 
+    #              message = "completing task...")
+    # updateTextInput(session, "pattern", value = "")
+    # updateTextInput(session, "replacement", value = "")
+    
+  })
+  
+  shiny::observeEvent(input$cancel, {
+    shiny::stopApp()
   })
   
 }
--- R/run_app.R
+++ R/run_app.R
@@ -3,5 +3,6 @@
 #' @export
 run_app <- function() {
   shiny::runGadget(app = shiny::shinyAppDir(system.file("app", package = "batchgsub")), 
+                   stopOnCancel = FALSE, 
                    viewer = shiny::dialogViewer('Batch file text replacement', height = 400))
 }
