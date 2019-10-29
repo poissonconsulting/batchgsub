6ce69c494c9f121362713138fb9aebff6688e957
Seb Dalgarno <seb@poissonconsulting.ca>
2019-10-29 14:52:43 -0700

    add path arg

--- R/run_app.R
+++ R/run_app.R
@@ -1,7 +1,9 @@
 #' Run the batch_gsub Shiny Application
 #'
+#' @param path A character string of the default path.
 #' @export
 run_app <- function(path = getwd()) {
+  chk::chk_string(path)
   shinyOptions(path = path)
   shiny::runGadget(app = shiny::shinyAppDir(system.file("app", package = "batchgsub")),
     viewer = shiny::dialogViewer("Batch file text replacement", height = 400))
